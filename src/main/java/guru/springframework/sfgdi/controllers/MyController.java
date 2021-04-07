b6abdba66d6c4e1d1cb2176bd325c0744353a1da
Marcin <45768431+marcinMarga@users.noreply.github.com>
2021-04-07 17:37:05 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        System.out.println(greetingService.sayGreeting());
+        return "Hi Folks !!!";
+    }
+}
---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+
+    private SetterInjectedController setterInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        setterInjectedController = new SetterInjectedController();
+        setterInjectedController.setGreetingService(new ConstructorGreetingService());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(setterInjectedController.getGreeting());
+    }
+}
