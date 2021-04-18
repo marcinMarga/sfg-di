d09aedfcaae7e64910ed357dfd5bfd5243822533
Marcin <45768431+marcinMarga@users.noreply.github.com>
2021-04-18 13:56:39 +0200

    Spring framework configuration II: -i18nService without bean name -pass in repository inteface to constructor

--- src/main/java/guru/springframework/sfgdi/configuration/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/configuration/GreetingServiceConfig.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgdi.configuration;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
 import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
 import guru.springframework.sfgdi.services.I18nPolishGreetingService;
@@ -8,16 +10,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
 @Configuration
 public class GreetingServiceConfig {
 
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+    }
+
     //@Service("i18nService")
     @Profile("EN")
     @Bean /*("i18nService")*/
-    I18nEnglishGreetingService i18nService(){
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
     //@Service("i18nService")
